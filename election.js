0a3886876fc4ae85b47c427902c700b4a9fc1c7c
heybrettbell <brett@grassrootsonline.ca>
2017-08-22 16:40:53 -0400

    Part 1 complete

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var resultsList = document.getElementById('results');
 
-  // Imagination!
+  $.ajax({
+    url: "https://bb-election-api.herokuapp.com/",
+    method: "GET",
+    data: {},
+    dataType: 'json'
+  }).done(function(responseData){
+      var candidateResults = responseData.candidates
+      for (var i = 0; i < candidateResults.length; i++) {
+        var candidateName = candidateResults[i]['name'];
+        var candidateVotes = candidateResults[i]['votes'];
 
+        var electionResults = document.createElement("li");
+        electionResults.innerText = candidateName + ": " + candidateVotes + " votes";
+        resultsList.appendChild(electionResults);
+      }
+  });
 });
